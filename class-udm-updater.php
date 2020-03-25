c99be7f8d040ddb08f51bbc49d293cf132898431
David Anderson <DavidAnderson684@users.noreply.github.com>
2020-03-25 14:36:21 +0000

    1.8.4

--- class-udm-updater.php
+++ class-udm-updater.php
@@ -9,7 +9,7 @@ Licence: MIT / GPLv2+
 if (!class_exists('Updraft_Manager_Updater_1_8')):
 class Updraft_Manager_Updater_1_8 {
 
-	public $version = '1.8.3';
+	public $version = '1.8.4';
 
 	public $relative_plugin_file;
 	public $slug;
